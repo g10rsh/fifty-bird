cd746545ca11683a1715affb4e5ce87ccbd7da43
Josh <g10rsh@gmail.com>
2021-12-04 10:36:12 +0900

    jump

--- birdOrigin/main.lua
+++ birdOrigin/main.lua
@@ -26,21 +26,30 @@ function love.load()
   push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
     vsync=true,fullscreen=false,resizable=true
   })
+
+  love.keyboard.keysPressed={}
 end
 
 function love.resize(w,h)
   push:resize(w,h)
 end
 
-function love.keyPressed(key)
-  if key=='escape' then
-    love.event.quit()
-  end
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed(key)
+  return love.keyboard.keysPressed[key]
 end
 
 function love.update(dt)
   backgroundScroll=(backgroundScroll+BACKGROUND_SCROLL_SPEED*dt)%BACKGROUND_LOOPING_POINT
   groundScroll=(groundScroll+GROUND_SCROLL_SPEED*dt)%GROUND_LOOPING_POINT
+  bird:update(dt)
+  love.keyboard.keysPressed={}
 end
 
 function love.draw()
